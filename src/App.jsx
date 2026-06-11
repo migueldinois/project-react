71ba255d4a17cc953dec1fe4b034c9cacea47c68
migueldinois <Miguel.dinois@aluno.senai.br>
2026-06-11 11:27:46 -0300

    feat: Finalização da logica base do contador

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,14 @@ function App() {
     <>
       {/* <Aluno nome={'Miguel'} idade={17} ativo={true} />
       <Aluno nome={'Carlos'} idade={23} ativo={false}/> */}
-      <Button texto={'Cadastro'} />
+      {/* <Button texto={'Cadastro'} />
       <Button texto={'Relatorio'} />
       <Card aluno={'Miguel'} curso={'CyberSecurity'} imagem={'https://placehold.co/400'}/>
       <Card aluno={'Carlos'} curso={'Cozinheiro'} imagem={'https://placehold.co/400'}/>
       <MsgUsuario nomeUsuario={'Miguel'} autenticado={true} />
       <MsgUsuario nomeUsuario={'Miguel'} autenticado={false} />
-      <UseStateHook/>
+      <UseStateHook/> */}
+      <Contador/>
     </>
   )
 };
--- src/components/Contador/Contador.jsx
+++ src/components/Contador/Contador.jsx
@@ -1,7 +1,36 @@
+import { use, useState } from 'react'
 import styles from './Contador.module.css'
 
-function Contador(){
+function Contador() {
 
+    const [numeroAtual, setNumeroAtual] = useState(0)
+
+    const aumentarContador = () => {
+        setNumeroAtual(numeroAtual + 1)
+    }
+
+    const diminuirContador = () => {
+        setNumeroAtual(numeroAtual - 1)
+    }
+
+    const reiniciarContador = () => {
+        setNumeroAtual(0)
+    }
+
+    
+
+    return (
+        <>
+            <div>
+                <p>{numeroAtual}</p>
+            </div>
+            <div>
+                <button onClick={diminuirContador}>Diminuir</button>
+                <button onClick={reiniciarContador}>Reiniciar</button>
+                <button onClick={aumentarContador}>Aumentar</button>
+            </div>
+        </>
+    )
 }
 
 export default Contador
